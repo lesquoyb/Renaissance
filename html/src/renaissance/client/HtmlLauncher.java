4278fbcad243ac837059f98a0cfdd6e006924236
Baptiste Lesquoy <nouvelleaddresse@yahoo.fr>
2015-01-23 21:33:42 +0100

    first commit
    lol

---
+++ html/src/renaissance/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package renaissance.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import renaissance.RenaissanceGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new RenaissanceGame();
+        }
+}
