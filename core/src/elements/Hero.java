1e16a02ceba3e4673941bc6637f123bdae3cb47a
Baptiste Lesquoy <nouvelleaddresse@yahoo.fr>
2015-01-24 17:02:22 +0100

    nanananana

--- core/src/elements/Hero.java
+++ core/src/elements/Hero.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Hero extends Personnages {
 
-	public static final int velocity = 150;
+	public static final int velocity = 180;
 	public static final float GRAVITE = 98.01f;
 	public Vector2 jump = new Vector2();
 	private static final Vector2 jumpUp = new Vector2(0,256);
